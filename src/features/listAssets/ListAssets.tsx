56f31bfa00ec3c449d29ade242291ca1748f8c47
Pablo Rodriguez <pablorodriguez833@gmail.com>
2022-09-28 20:32:05 -0300

    feat: handle onRowClik on table

--- src/features/listAssets/ListAssets.tsx
+++ src/features/listAssets/ListAssets.tsx
@@ -1,5 +1,6 @@
 import { Table } from "../../components/table";
 import { useGetCoins } from "../../hooks";
+import { CoinType } from "../../models/coins.type";
 
 const numberFormat = (value: number) => {
   return new Intl.NumberFormat("en-US", {
@@ -8,14 +9,21 @@ const numberFormat = (value: number) => {
   }).format(value);
 };
 
+const headers = {
+  name: "Coin",
+  current_price: "Price",
+  market_cap: "Market Cap",
+};
+
 export const ListAssests = () => {
   const { data, error, isLoading } = useGetCoins();
 
-  const headers = {
-    name: "Coin",
-    current_price: "Price",
-    market_cap: "Market Cap",
+  const onRowClick = (item: CoinType) => {
+    console.log(
+      "turboCL -> file: ListAssets.tsx -> line 15 -> onRowClick -> item",
+      item
+    );
   };
 
-  return <Table headers={headers} data={data} />;
+  return <Table headers={headers} data={data} onRowClick={onRowClick} />;
 };
