ee9dd1d700b5bf8d7554b2ef90d5cfb5eb920bc3
Pablo Rodriguez <pablorodriguez833@gmail.com>
2022-09-28 02:09:12 -0300

    chore: remove console.log

--- src/services/coins.ts
+++ src/services/coins.ts
@@ -4,7 +4,6 @@ const API_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1";
 
 export const getCoins = async (): Promise<CoinType[]> => {
-  console.log("pablo");
   return fetch(API_URL)
     .then((response) => {
       return response.json();
